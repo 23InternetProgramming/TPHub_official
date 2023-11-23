3fba5809f800dcb923f371a818b526fb2e077ab1
edle607 <govldnflwlq@naver.com>
2023-11-23 12:35:54 +0900

    board/etc/landing_page/member/schedule/self_profile 생성 및 board 백 작업

---
+++ board/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+import os
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    hook_text = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/board/category/{self.slug}'
+
+# User 모델을
+# Subclassing (하위 클래스화): 기존 User 모델을 상속하고 새로운 필드를 추가하는 방법입니다.
+# 방법으로 전화번호, 학번, 프로필 이미지 등 새로운 필드 넣기
+# member App에서 해야하나..
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    # 현재 로그인된 유저를 자동으로 가져와야 함
+    author = models.ForeignKey(User, null=True, blank=False, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=False, on_delete=models.SET_NULL)
+
+    content = models.TextField()
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
+    # 현재 로그인된 유저의 프로필 이미지를 가져와야 함
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        truncated_content = self.content[:10] + '...' if len(self.content) > 10 else self.content
+        return f'[{self.pk}] | {self.category} | {self.author}) {self.title} : {truncated_content}'
+
+    def change_ordering(self):
+        self.ordering = 'pk' if self.ordering_state else '-pk'
+        self.ordering_state = not self.ordering_state
+
+    def get_absolute_url(self):
+        return f'/board/category/{self.category}/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
