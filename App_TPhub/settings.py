5c957c9de62d7902d646ce3d67f391116c6d9568
agape1018 <kate3066@gmail.com>
2023-11-23 18:20:40 +0900

    settings.py 수정 static

--- App_TPhub/settings.py
+++ App_TPhub/settings.py
@@ -124,6 +124,10 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
