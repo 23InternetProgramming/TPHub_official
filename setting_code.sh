ebd4174aa1efc095e67c6d87f580efc64b461506
edle607 <govldnflwlq@naver.com>
2023-11-23 12:17:08 +0900

    App_TPhub 생성 및 초기 환경 설정

---
+++ setting_code.sh
@@ -0,0 +1,13 @@
+echo "----------------pip install django-----------------"
+pip install django
+echo "--------python –m pip install --upgrade pip--------"
+python -m pip install --upgrade pip
+echo "----------pip install –r requirements.txt----------"
+pip install -r requirements.txt
+echo "----pip install --upgrade pip setuptools wheel-----"
+pip install --upgrade pip setuptools wheel
+echo "-----------python –m pip install pillow -----------"
+python -m pip install pillow
+echo "------------pip install beautifulsoup4-------------"
+pip install beautifulsoup4
+
