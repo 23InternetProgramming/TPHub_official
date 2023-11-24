4eeddfd6ac1d35fce07ae742b446f159dbd32271
agape1018 <142515160+agape1018@users.noreply.github.com>
2023-11-24 20:57:59 +0900

    KSY 회원가입 기능 구현 (#14)

---
+++ account/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=32, verbose_name='계정이름')
+    password = models.CharField(max_length=32, verbose_name='비밀번호')
+    registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'test_user'
