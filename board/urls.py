0bbfde2a72a636b2a6aaecb0dc7e06e3bc74dcf9
edle607 <govldnflwlq@naver.com>
2023-11-24 10:04:25 +0900

    YHJ - 포스트 업데이트 기능

--- board/urls.py
+++ board/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     #path('category/', views.CategoryList.as_view()), # 카테고리 종류 4개 보일 화면
     #path('category/<str:slug>/', views.category_post), # 해당 카테고리로 쓰여진 모든 글 목록
     #path('category/<str:slug>/<int:pk>/', views.PostDetail.as_view()), # 글 상세보기
-    #path('create_post/', views.PostCreate.as_view()),
+    #path('create_post/', views.PostCreate.as_view()), # 글 생성
+    #path('update_post/<int:pk>/', views.PostUpdate.as_view()), # 글 수정
 ]
