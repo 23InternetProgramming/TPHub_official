9752534bd997463b9024829e5fc12cdf7a9489f5
edle607 <120429435+edle607@users.noreply.github.com>
2023-11-30 20:30:23 +0900

    YHJ - 2주차 1차 머지 완료 (#22)
    * YHJ - 포스트 상세 페이지에 댓글 나타내기
    
    * YHJ - 댓글 모달띄워서 삭제 기능 추가
    
    * YHJ - 댓글 테스트
    
    * YHJ - schedule 달력 기능 완성
    
    * YHJ - board의 포스트 정렬 기능
    
    * YHJ - todolist 개발 중
    
    * YHJ - post_detail 수정
    
    * YHJ - 11/30 2주차 1차 머지 완료

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,6 +1,15 @@
+from django.conf.urls import url
 from django.urls import path
-from . import views
 
+from . import views
+from .views import EventDeleteView
+
+app_name = 'schedule'
 urlpatterns = [
-    path('', views.PostList.as_view())
+    path('index', views.index, name='index'),
+    path('createTodo', views.createTodo, name="createTodo"),
+    url(r'^$', views.CalendarView.as_view(), name='calendar'),
+    url(r'^event/new/$', views.event, name='event_new'),
+    url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
+    url(r'^event/delete/(?P<pk>\d+)/$', EventDeleteView.as_view(), name='event_delete'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,7 +1,94 @@
-from django.views.generic import ListView
-from .models import Post
+from calendar import calendar
+import calendar
+from datetime import datetime, date, timedelta
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.utils.safestring import mark_safe
 
+from .forms import EventForm
+from .models import *
+from .utils import Calendar
 
-class PostList(ListView) :
-    model = Post
-    ordering = '-pk'
+def index(request) :
+    todos = Todo.objects.all()
+    print("From DB: ", todos)
+    content = {'todos' : todos}
+    return render(request, "schedule/index.html", content)
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content=user_input_str)
+    # author이 자동으로 채워지도록 코드 추가
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+
+class EventDeleteView(generic.DeleteView):
+    model = Event
+    template_name = 'schedule/event_confirm_delete.html'
+    success_url = '/schedule/' #reverse_lazy('schedule:calendar')
+
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'schedule/calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        today = datetime.today().date()
+        d = get_date(self.request.GET.get('month', str(today.year) + '-' + str(today.month))) ## ('day', None)
+
+        # Instantiate our calendar class with today's year and date
+        schedule = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_schedule = schedule.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_schedule)
+
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+
+        ##
+        # Pass the URLs with updated month to the template
+        context['prev_month_url'] = f"{reverse('schedule:calendar')}?{context['prev_month']}"
+        context['next_month_url'] = f"{reverse('schedule:calendar')}?{context['next_month']}"
+
+        # Pass today's date to the template
+        context['today'] = today
+
+        return context
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
+
+def event(request, event_id=None):
+    instance = Event()
+    if event_id:
+        instance = get_object_or_404(Event, pk=event_id)
+    else:
+        instance = Event()
+
+    form = EventForm(request.POST or None, instance=instance)
+    if request.POST and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('schedule:calendar'))
+    return render(request, 'schedule/event.html', {'form': form})
+
