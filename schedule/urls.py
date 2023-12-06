d40bcd2a45c66c0070baba1b7502e83370433464
urapp13ofmyeye <145529622+urapp13ofmyeye@users.noreply.github.com>
2023-12-06 14:29:33 +0900

    PSH - 1206 1차 PR (#35)
    * PSH - 1127 수업중
    
    * PSH - 1130 로그인 및 회원가입
    
    * PSH - 1130 PR 중 수정
    
    * PSH - 1203 커밋
    
    * PSH -1204 수업중
    
    * PSH -1205 저녁회의
    
    * PSH - 1205 저녁회의
    
    * PSH - 1206 1차 PR

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import url
 from django.urls import path
 
 from . import views
@@ -9,8 +8,8 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('createTodo', views.create_todo, name='createTodo'),
     path('deleteTodo', views.deleteTodo, name='deleteTodo'),
-    url(r'^$', views.CalendarView.as_view(), name='calendar'),
-    url(r'^event/new/$', views.event, name='event_new'),
-    url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
-    url(r'^event/delete/(?P<pk>\d+)/$', EventDeleteView.as_view(), name='event_delete'),
+    path('', views.CalendarView.as_view(), name='calendar'),  # 수정된 부분
+    path('event/new/', views.event, name='event_new'),  # 수정된 부분
+    path('event/edit/<int:event_id>/', views.event, name='event_edit'),  # 수정된 부분
+    path('event/delete/<int:pk>/', EventDeleteView.as_view(), name='event_delete'),  # 수정된 부분
 ]
