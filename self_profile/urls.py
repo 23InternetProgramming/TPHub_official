02a852246ebaedbdb7977ff5acbee46b743635e0
edle607 <120429435+edle607@users.noreply.github.com>
2023-12-06 23:14:09 +0900

    YHJ - 1206 제출 코드 (#36)
    * YHJ - 포스트 상세 페이지에 댓글 나타내기
    
    * YHJ - 댓글 모달띄워서 삭제 기능 추가
    
    * YHJ - 댓글 테스트
    
    * YHJ - schedule 달력 기능 완성
    
    * YHJ - board의 포스트 정렬 기능
    
    * YHJ - todolist 개발 중
    
    * YHJ - post_detail 수정
    
    * YHJ - 11/30 2주차 1차 머지 완료
    
    * YHJ - 11/30 머지 중
    
    * YHJ - 11/30 머지 중 수정
    
    * YHJ - 11/30 머지 했지만 이상한 오류. 하지만 서버 돌아감
    
    * YHJ - todolist 삭제 구현
    
    * YHJ - todolist 유저별로 보이도록 구현
    
    * YHJ - 대문 페이지에 개인일정 연동 중
    
    * YHJ - 1204 PR
    
    * YHJ -인증된 상태일때만 스케쥴/게시글 보이게 수정
    
    * YHJ - todo 대문페이지에 연결
    
    * YHJ - 대문 페이지 달력 위젯 작업중
    
    * YHJ - 1205 2주차 2차 PR
    
    * YHJ - 대문페이지 위젯연결 및 프론트 자잘하게 수정
    
    * YHJ 1206
    
    * YHJ - 제출 코드

--- self_profile/urls.py
+++ self_profile/urls.py
@@ -5,7 +5,8 @@ from .views import view_profile, user_profile_update_form
 app_name = 'self_profile'
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
+    #path('', views.PostList.as_view()),
+    path('', views.view_profile),
     path('view_profile/', views.view_profile, name='view_profile'),
     path('user_profile_update_form/', user_profile_update_form, name='user_profile_update_form'),
 ]
